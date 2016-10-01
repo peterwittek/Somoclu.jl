e269a7c380991dc8065177605d7338044c52244e
Peter Wittek <peterwittek@users.noreply.github.com>
2016-10-01 18:29:34 +0200

    Dummy changes to trigger Travis build

--- deps/build.jl
+++ deps/build.jl
@@ -5,9 +5,7 @@ using BinDeps
 link = "https://github.com/peterwittek/somoclu/releases/download/1.7.0/somoclu-1.7.0-julia-test.tar.gz"
 
 libsomoclu = library_dependency("libsomoclu")
-
 provides(Sources, Dict(URI(link) => libsomoclu))
-
 provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsomoclu.so"), configure_options=[AbstractString("--without-mpi")]), libsomoclu)
 
 @BinDeps.install Dict(:libsomoclu => :libsomoclu)
