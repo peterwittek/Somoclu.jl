d0b0f16d84eed3d7c9880532257733bbd2273306
Peter Wittek <peterwittek@users.noreply.github.com>
2016-10-01 18:22:12 +0200

    Testing macOS build

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
-link = "https://github.com/peterwittek/somoclu/releases/download/1.7.0/somoclu-1.7.0.tar.gz"
+link = "https://github.com/peterwittek/somoclu/releases/download/1.7.0/somoclu-1.7.0-julia-test.tar.gz"
 
 libsomoclu = library_dependency("libsomoclu")
 
