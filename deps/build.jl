70d06f2acdbf83162ac02b89025ab17114c4d4b2
Peter Wittek <peterwittek@users.noreply.github.com>
2016-11-11 15:47:16 +0100

    Somoclu download link updated

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
-link = "https://github.com/peterwittek/somoclu/releases/download/1.7.1/somoclu-1.7.1.tar.gz"
+link = "https://github.com/peterwittek/somoclu/releases/download/1.7.1/somoclu-1.7.2-pre.tar.gz"
 libsomoclu = library_dependency("libsomoclu", aliases=["libsomoclu", "libsomoclu.so"], os=:Unix)
 provides(Sources, Dict(URI(link) => libsomoclu))
 provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsomoclu.so"), configure_options=[AbstractString("--without-mpi")]), libsomoclu, os = :Unix)
