a24466eb98131fe160a682d4b26ea94405aba95d
Peter Wittek <peterwittek@users.noreply.github.com>
2016-11-16 16:37:19 +0100

    Decreased test accuracy

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ function deterministic_codebook()
     data = Array{Float32}([0.1 0.2; 0.3 0.4]);
     bmus, umatrix = train!(codebook, data, ncolumns, nrows);
     correct_codebook = Array{Float32, 2}([0.15  0.126894  0.173106  0.15; 0.35  0.326894  0.373106  0.35]);
-    return sum(codebook - correct_codebook) < 10e-8
+    return sum(codebook - correct_codebook) < 10e-6
 end
 
 # write your own tests here
