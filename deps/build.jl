c005a4b223efcac9f053368b0698aed4bcd43762
Peter Wittek <peterwittek@users.noreply.github.com>
2018-02-12 20:08:50 +1000

    Reverted back to libsomoclu

--- deps/build.jl
+++ deps/build.jl
@@ -4,18 +4,18 @@ using BinDeps
 
 somoclu_version="1.7.4"
 
-link = "https://github.com/peterwittek/somoclu/releases/download/$somoclu_version/somoclu-$(somoclu_version).tar.gz"
+link = "https://github.com/peterwittek/somoclu/releases/download/1.7.5-pre/somoclu-$(somoclu_version).tar.gz"
 
 println(link)
 
 if !is_windows()
-	libjlsomoclu = library_dependency("libjlsomoclu",
-								 aliases=["libjlsomoclu", "libjlsomoclu.so"], os=:Unix)
-	provides(Sources, Dict([URI(link) => libjlsomoclu]))
+	libsomoclu = library_dependency("libsomoclu",
+								 aliases=["libsomoclu", "libsomoclu.so"], os=:Unix)
+	provides(Sources, Dict([URI(link) => libsomoclu]))
 	provides(BuildProcess,
-			Autotools(libtarget = joinpath("src", "libjlsomoclu.so"),
-			configure_options=[AbstractString("--without-mpi")]), libjlsomoclu, os=:Unix)
-	@BinDeps.install Dict([:libjlsomoclu => :libjlsomoclu])
+			Autotools(libtarget = joinpath("src", "libsomoclu.so"),
+			configure_options=[AbstractString("--without-mpi")]), libsomoclu, os=:Unix)
+	@BinDeps.install Dict([:libsomoclu => :libsomoclu])
 else
     using WinRPM
 	using Base.Filesystem
@@ -31,37 +31,37 @@ else
 
 	GCCROOT = joinpath(Pkg.dir("WinRPM"),"deps")
 
-	libjlsomoclu = library_dependency("libjlsomoclu",
-								 aliases=["libjlsomoclu", "libjlsomoclu.dll"],
+	libsomoclu = library_dependency("libsomoclu",
+								 aliases=["libsomoclu", "libsomoclu.dll"],
 								 os=:Windows)
-	provides(Sources, Dict([URI(link) => libjlsomoclu]))
+	provides(Sources, Dict([URI(link) => libsomoclu]))
 
 
 	makedeplnk = "https://sourceforge.net/projects/gnuwin32/files/make/3.81/make-3.81-dep.zip/download"
 	makebinlnk = "https://sourceforge.net/projects/gnuwin32/files/make/3.81/make-3.81-bin.zip/download"
 
-	makedepzip 	= joinpath(BinDeps.downloadsdir(libjlsomoclu), "make-3.81-dep.zip")
-	makebinzip   = joinpath(BinDeps.downloadsdir(libjlsomoclu), "make-3.81-bin.zip")
-	makebuilddir = joinpath(BinDeps.builddir(libjlsomoclu),  "make")
+	makedepzip 	= joinpath(BinDeps.downloadsdir(libsomoclu), "make-3.81-dep.zip")
+	makebinzip   = joinpath(BinDeps.downloadsdir(libsomoclu), "make-3.81-bin.zip")
+	makebuilddir = joinpath(BinDeps.builddir(libsomoclu),  "make")
 
 	make_s, make_d =
 		joinpath(makebuilddir, "bin", "make.exe"),
-		joinpath(BinDeps.bindir(libjlsomoclu), "make.exe")
+		joinpath(BinDeps.bindir(libsomoclu), "make.exe")
 	icnv_s, icnv_d =
 		joinpath(makebuilddir, "bin", "libiconv2.dll"),
-		joinpath(BinDeps.bindir(libjlsomoclu), "libiconv2.dll")
+		joinpath(BinDeps.bindir(libsomoclu), "libiconv2.dll")
 	intl_s, intl_d =
 		joinpath(makebuilddir, "bin", "libintl3.dll"),
-		joinpath(BinDeps.bindir(libjlsomoclu), "libintl3.dll")
+		joinpath(BinDeps.bindir(libsomoclu), "libintl3.dll")
 
-	somoclusrcdir   = joinpath(BinDeps.srcdir(libjlsomoclu),   "somoclu-$somoclu_version")
-	somoclubuilddir = joinpath(BinDeps.builddir(libjlsomoclu), "somoclu-$somoclu_version")
-	somoclumakefile = joinpath(BinDeps.depsdir(libjlsomoclu),  "Makefile.libsomoclu.mingw")
+	somoclusrcdir   = joinpath(BinDeps.srcdir(libsomoclu),   "somoclu-$somoclu_version")
+	somoclubuilddir = joinpath(BinDeps.builddir(libsomoclu), "somoclu-$somoclu_version")
+	somoclumakefile = joinpath(BinDeps.depsdir(libsomoclu),  "Makefile.libsomoclu.mingw")
 
 	provides(SimpleBuild,
 		(@build_steps begin
-			CreateDirectory(BinDeps.bindir(libjlsomoclu))
-			FileRule(joinpath(BinDeps.bindir(libjlsomoclu), "make.exe"), @build_steps begin
+			CreateDirectory(BinDeps.bindir(libsomoclu))
+			FileRule(joinpath(BinDeps.bindir(libsomoclu), "make.exe"), @build_steps begin
 				FileDownloader(makedeplnk, makedepzip)
 				FileDownloader(makebinlnk, makebinzip)
 				FileUnpacker(makedepzip, makebuilddir, joinpath("bin", "libiconv2.dll"))
@@ -72,7 +72,7 @@ else
 					cp(intl_s, intl_d, remove_destination=true)
 				end
 			end)
-			GetSources(libjlsomoclu)
+			GetSources(libsomoclu)
 			CreateDirectory(somoclubuilddir)
 			@build_steps begin
 				ChangeDirectory(somoclubuilddir)
@@ -82,14 +82,14 @@ else
 					   remove_destination=true)
 					end)
 				`../../usr/bin/make.exe ARCH=$(Sys.ARCH) version=$(somoclu_version)`
-				CreateDirectory(BinDeps.libdir(libjlsomoclu))
-				FileRule(joinpath(BinDeps.usrdir(libjlsomoclu),"lib","libjlsomoclu.dll"), ()->
-					cp("libjlsomoclu.dll", joinpath(BinDeps.libdir(libjlsomoclu), "libjlsomoclu.dll"),
+				CreateDirectory(BinDeps.libdir(libsomoclu))
+				FileRule(joinpath(BinDeps.usrdir(libsomoclu),"lib","libsomoclu.dll"), ()->
+					cp("libsomoclu.dll", joinpath(BinDeps.libdir(libsomoclu), "libsomoclu.dll"),
 					   remove_destination=true))
 			end
-		end), libjlsomoclu, os=:Windows)
+		end), libsomoclu, os=:Windows)
 
     push!(BinDeps.defaults, SimpleBuild)
-	@BinDeps.install Dict([:libjlsomoclu => :libjlsomoclu])
+	@BinDeps.install Dict([:libsomoclu => :libsomoclu])
     pop!(BinDeps.defaults)
 end
